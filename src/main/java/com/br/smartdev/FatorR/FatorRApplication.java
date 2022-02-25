ef86354b720d0fe0c28f86ba92f5228cf68e7df0
Fernandak <f-fernanda@outlook.com>
2022-02-25 19:11:48 -0300

    Commit inicial

---
+++ src/main/java/com/br/smartdev/FatorR/FatorRApplication.java
@@ -0,0 +1,13 @@
+package com.br.smartdev.FatorR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatorRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatorRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/smartdev/FatorR/FatorRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.smartdev.FatorR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatorRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
